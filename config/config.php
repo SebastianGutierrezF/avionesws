42c976ba58f5d5296f2e003f9e6ea56e2aeec419
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2023-09-21 19:27:31 -0600

    Change db

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-const host = 'localhost';
-const user = '539909';
-const psw = '12345678';
-const db = '539909';
-const charset = 'utf8';
-const key = 'claseRapida';
+    const host = 'us-cdbr-east-06.cleardb.net';
+    const user = 'b5f432bb394cee';
+    const psw = 'b5f432bb394cee';
+    const db = 'heroku_211eb8b870228bb';
+    const charset = 'utf8';
+    const key = 'claseRapida';
 ?>
