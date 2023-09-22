97bc9fbce2a3c196c27da8786a57fe2408de32f1
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2023-09-21 19:29:52 -0600

    Fix psw

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
     const host = 'us-cdbr-east-06.cleardb.net';
     const user = 'b5f432bb394cee';
-    const psw = 'b5f432bb394cee';
+    const psw = 'a587caeb';
     const db = 'heroku_211eb8b870228bb';
     const charset = 'utf8';
     const key = 'claseRapida';
