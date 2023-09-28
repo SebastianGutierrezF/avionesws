1d9e8084c13cb8dcab6fb24fdb070874765e3885
Sebastian Gutierrez <l.gufs980514@itses.edu.mx>
2023-09-28 12:31:44 -0600

    Use db transaction for sign up

--- models/usuario.php
+++ models/usuario.php
@@ -1,34 +1,49 @@
 <?php
-namespace models;
-use main\db_main as db;
-class usuario {
 
-    public static function register($body) {
+namespace models;
+
+use main\db_main as db;
+
+class usuario
+{
+    // Utiliza una transacción para ejectuar dos consultas
+    // La primera inserta al usuario y la segunda le da el permiso default de usuario
+    public static function register($body)
+    {
         $hashPsw = password_hash($body->passU, PASSWORD_DEFAULT);
-        $sql = (object) [
-            'query' => 'INSERT INTO usuario(nombresU, apellidoUPaterno, apellidoUMaterno, correoU, passU, sexo, fechaNacimientoU) VALUES(?, ?, ?, ?, ?, ?, ?);',
-            'params' => [
-                $body->nombresU, 
-                $body->apellidoUPaterno, 
-                $body->apellidoUMaterno, 
-                $body->correoU, 
-                $hashPsw, 
-                $body->sexo,
-                $body->fechaNacimientoU
+        $sqls = (object) [
+            (object) [
+                'query' => 'INSERT INTO usuario(nombresU, apellidoUPaterno, apellidoUMaterno, correoU, passU, sexo, fechaNacimientoU) VALUES(?, ?, ?, ?, ?, ?, ?);',
+                'params' => [
+                    $body->nombresU,
+                    $body->apellidoUPaterno,
+                    $body->apellidoUMaterno,
+                    $body->correoU,
+                    $hashPsw,
+                    $body->sexo,
+                    $body->fechaNacimientoU
                 ]
+            ],
+            (object) [
+                'query' => 'INSERT INTO usuarioperfil(idUsuario, idPerfil) VALUES(LAST_INSERT_ID(), 14);',
+                'params' => []
+            ]
         ];
-        return db::save($sql);
+        return db::save_transaction_PDO($sqls);
     }
-    
-    public static function accountExists($body) {
+
+    // Checa si la cuenta existe
+    public static function accountExists($body)
+    {
         $sql = (object) [
             'query' => 'SELECT * FROM usuario WHERE correoU = ?;',
             'params' => [$body->correoU]
         ];
-        return (db::queryOne($sql)->rowCount() > 0);
+        return (count((array)db::queryOne($sql)) > 0);
     }
-    
-    public static function changePsw($body) {
+
+    public static function changePsw($body)
+    {
         $hashPsw = password_hash($body->passU, PASSWORD_DEFAULT);
         $sql = (object) [
             'query' => 'UPDATE usuario SET passU = ? WHERE correoU = ?;',
@@ -37,4 +52,3 @@ class usuario {
         return db::save($sql);
     }
 }
-?>
